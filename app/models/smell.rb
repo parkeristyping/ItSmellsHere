4f8078ce08066f9858746e46315a70443a19e1b5
Parker Lawrence <parker.alford@gmail.com>
2015-05-28 18:03:15 -0400

    add front-end styling

--- app/models/smell.rb
+++ app/models/smell.rb
@@ -1,3 +1,21 @@
 class Smell < ActiveRecord::Base
   belongs_to :user
+
+  def time_ago
+    time_ago = Time.now - self.created_at
+    if time_ago < 60
+      " just now"
+    elsif time_ago < 60 * 60
+      " #{(time_ago/60).to_i} minutes ago"
+    elsif time_ago < 60 * 60 * 2
+      " #{(time_ago/(60 * 60)).to_i} hour ago"
+    elsif time_ago < 60 * 60 * 24
+      " #{(time_ago/(60 * 60)).to_i} hours ago"
+    elsif time_ago < 60 * 60 * 24 * 2
+      " yesterday"
+    else
+      " #{(@time_ago/(60 * 60 * 24)).to_i} days ago"
+    end
+  end
+
 end
